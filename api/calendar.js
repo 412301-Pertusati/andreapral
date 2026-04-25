b2bafc1a03dfb49e29dad2ab42015f849bf03350
412301-Pertusati <412301@tecnicatura.frc.utn.edu.ar>
2026-04-25 15:22:19 -0300

    fix in vercel

--- api/calendar.js
+++ api/calendar.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Solo GET
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Método no permitido' });
